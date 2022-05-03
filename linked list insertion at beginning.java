3ec7b43c8f9493c1af594a803ec85019cecfc0b2
Prashanth-2002 <103819382+Prashanth-2002@users.noreply.github.com>
2022-05-03 04:21:14 -0700

    Add files via upload

---
+++ linked list insertion at beginning.java
@@ -0,0 +1,95 @@
+import java.util.*;
+
+
+public class ll {
+     public Node head = null;  
+     public Node tail = null;    
+    
+    class Node{
+        int data;
+        Node next;
+    
+        Node(int data) {this.data = data; this.next = null; }
+
+    }
+       
+    
+     public void addAtStart(int data) {  
+     Node newnode = new Node(data);  
+        
+        if(head==null){
+          head = newnode;  
+         tail = newnode;    
+        }
+        else {
+            newnode.next=head;
+             head=newnode;
+        }
+    }
+    public void display() {  
+        Node current = head;  
+        if(head == null) {  
+        System.out.println("List is empty");  
+        return;  
+        }  
+        System.out.println("Adding nodes to the start of the list: ");  
+        while(current != null) {  
+        System.out.print(current.data + " ");  
+        current = current.next;  
+        }  
+        System.out.println();  
+        }  
+        
+    
+    public static void main(String[] args) {  
+
+        Main sList = new Main();  
+        
+        sList.addAtStart(1);  
+        sList.display();  
+        
+        sList.addAtStart(2);  
+        sList.display();  
+        
+        sList.addAtStart(3);  
+        sList.display();  
+        
+        sList.addAtStart(4);  
+        sList.display();  
+        }  
+        } 
+    
+    
+
+
+
+
+
+
+      
+
+
+
+
+    
+    
+
+       
+    
+    
+
+
+    
+
+    
+
+
+
+
+
+        
+
+
+
+    
+
